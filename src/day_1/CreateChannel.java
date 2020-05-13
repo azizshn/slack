53d9f499fb655af7aa5672ac51f5c443d7442b1e
azizshn <azizsahin@yandex.com>
2020-05-13 15:57:46 +0100

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,10 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create channels");
+
+        int x = 0; //change from itellij
+        if (x == 0) {
+            System.out.println("equal");
+        }
     }
 }
